dcaf17660ec50c9ed0e7ea31c0a0b7eb0cf64d83
mmufasa <86222656+mmufasa@users.noreply.github.com>
2022-11-26 12:47:53 +0800

    Update library.js

--- library.js
+++ library.js
@@ -58,3 +58,17 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true
+})
+
+sr.reveal(`.home__data`)
+sr.reveal(`.home__img`, {delay: 500})
+sr.reveal(`.home__social`, {delay: 600})
+sr.reveal(`.mv__card, .section__title-center , .officers__card, .footer`,{interval: 100})
